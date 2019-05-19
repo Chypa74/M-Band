baa0f1b2b5af98b6795f409252bfc92d78ce6e13
Dmitry Shuvalov <mitiashuv74@mail.ru>
2019-05-19 23:23:02 +0300

    Remove comment model

--- models/CommentModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const CommentSchema = require('../schemas/CommentSchema');
-
-const CommentModel = mongoose.model('Comment', CommentSchema);
-
-module.exports = CommentModel;
--- schemas/PostSchema.js
+++ schemas/PostSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const CommentSchema = require('./CommentSchema');
 
 const Schema = mongoose.Schema;
 
@@ -7,7 +8,7 @@ const PostSchema = new Schema({
   tags: [String],
   content: String,
   likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+  comments: [CommentSchema]
 });
 
 module.exports = PostSchema;
